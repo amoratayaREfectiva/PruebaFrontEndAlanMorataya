747fb155678d7de3c9d22bb811afe737835684f0
amoratayaREfectiva <amorataya@redefectiva.com>
2024-12-12 01:42:53 -0600

    fix textos

--- FrontEnd/crud-app/src/Components/PersonaListHeader.js
+++ FrontEnd/crud-app/src/Components/PersonaListHeader.js
@@ -3,14 +3,14 @@ import React from 'react';
 const PersonaListHeader = ({ onCreate }) => (
   <div className="row">
     <div className="col-6" style={{ display: 'flex', marginBottom: '10px' }}>
-      <h2>Listado de usuarios</h2>
+      <h2>Listado de Personas</h2>
     </div>
     <div
       className="col-6"
       style={{ display: 'flex', justifyContent: 'flex-end', marginBottom: '10px' }}
     >
       <button type="button" className="btn btn-success" onClick={onCreate}>
-        Crear Usuario
+        Crear Persona
       </button>
     </div>
   </div>
